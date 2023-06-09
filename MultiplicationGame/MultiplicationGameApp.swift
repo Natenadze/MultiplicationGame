19561ffaf653c06302b3035465fafffa0bbd4c04
davit <davit.natenadze@gmail.com>
2023-06-09 11:52:45 +0200

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Davit Natenadze on 09.06.23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
